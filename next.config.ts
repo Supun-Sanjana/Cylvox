7db608ccda81ace41ee0b47f7b5f5302508fdc8a
Supun <infor.ssupun@gmail.com>
2026-08-03 23:41:06 +0530

    fix: update base URL in seo.ts and remove redirects from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "https://cylvox.com/:path*",
-        statusCode: 301,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.cylvox.com",
-          },
-        ],
-        destination: "https://cylvox.com/:path*",
-        statusCode: 301,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
